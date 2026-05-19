559d971139f6a63485f9b35e11ccf6f9408e6bfc
Valentina <Valentinavalerohernandez@gmail.com>
2026-05-19 16:52:14 -0300

    Etapa 5 concluida!

--- js/locaisDoacao.js
+++ js/locaisDoacao.js
@@ -4,3 +4,44 @@ function goToHospital(nome, cidade, imagem) {
 
     window.location.href = url;
 }
+
+
+// PESQUISA
+const searchInput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".card");
+const noResults = document.getElementById("noResults");
+
+searchInput.addEventListener("input", () => {
+
+    const value = searchInput.value.toLowerCase();
+
+    let visibleCards = 0;
+
+    cards.forEach(card => {
+
+        const hospital =
+            card.querySelector("h3").textContent.toLowerCase();
+
+        const cidade =
+            card.querySelector("p").textContent.toLowerCase();
+
+        if (
+            hospital.includes(value) ||
+            cidade.includes(value)
+        ) {
+            card.style.display = "block";
+            visibleCards++;
+        } else {
+            card.style.display = "none";
+        }
+
+    });
+
+    // MOSTRAR MENSAGEM
+    if (visibleCards === 0) {
+        noResults.style.display = "block";
+    } else {
+        noResults.style.display = "none";
+    }
+
+});
